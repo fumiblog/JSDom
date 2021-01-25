00d9cc3a97435863f54249900fcd6680c47acb66
矢野文久 <fumiblog@yanobunkyuunoMacBook-Pro.local>
2021-01-25 12:56:09 +0900

    [First] 1-1

---
+++ README.md
@@ -0,0 +1,7 @@
+# JavaScript DOM編
+
+## 3st JavaScript
+
+### ドットインストール　https://dotinstall.com/lessons/basic_javascript_dom_v2
+
+#### 2021.1.25 Monday Sunny
---
+++ js/main.js
@@ -0,0 +1,10 @@
+'use strict'
+{
+  //2.要素を操作してみよう
+  function update() {
+    document.getElementById('target').textContent = "Changed!";
+  }
+  setTimeout(update, 1000);
+
+  //1.DOM について理解しよう
+}
